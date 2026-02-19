a420d70be8668edfd3ee936726d0f1be1e2be04b
SHIVA KUMAR <151666163+Hpshiva@users.noreply.github.com>
2026-02-19 19:46:59 +0000

    data types convertions

---
+++ 01_basic_16th_feb_2026/Day_1/test.js
@@ -0,0 +1,18 @@
+const a=3;
+console.log(a)
+
+
+let name="shiva"
+console.log(name);
+
+{
+var age=27
+console.table([a,name,age]);
+}
+console.log(age)
+  
+
+age=33
+// a=33
+
+console.table([a,name,age]);
---
+++ 01_basic_16th_feb_2026/Day_2/datatypes.js
@@ -0,0 +1,20 @@
+// data converting into Number and checking the typeof of it 
+
+// Number
+let a = 33
+console.table([a,typeof(a)])
+
+
+// String
+let age="43shiva";
+let ageNum=Number(age)
+console.table([age , typeof(age),ageNum , typeof(ageNum)])
+
+
+// Boolean
+let check=true
+let checkNum=Number(check)
+console.table([check,typeof(check),checkNum,typeof(checkNum)])
+
+
+// 
