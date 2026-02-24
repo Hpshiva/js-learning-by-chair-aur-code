e6a3e0b2dafd19c3d9cbfbcdbe5d79d0e6faf928
SHIVA KUMAR <151666163+Hpshiva@users.noreply.github.com>
2026-02-24 12:58:14 +0000

    math

---
+++ 01_basic_16th_feb_2026/Day_4 (number)/Math.js
@@ -0,0 +1,18 @@
+console.log(Math)
+
+// automaticly 
+console.log(Math.round(4.2))
+console.log(Math.round(4.6))
+
+let num=2
+
+//manutally 
+console.log(Math.ceil(4.01))
+console.log(Math.floor(4.9))
+
+
+//.random always btn  0 to 1
+console.log(Math.random())
+
+console.log(Math.round((Math.random()*10)+1))
+
