dfd619604a16bbed27f6c62030fdbfa8913aed2c
SHIVA KUMAR <151666163+Hpshiva@users.noreply.github.com>
2026-02-26 19:15:47 +0000

    splice and slice

---
+++ 02_basic/Array_start.js
@@ -0,0 +1,18 @@
+let arr=[1,2,3,4,5,6]
+  
+
+// let arr1=arr.join()
+
+
+// console.log(arr)
+// console.log(typeof arr)
+
+// console.log(arr1)
+// console.log(typeof arr1)
+
+
+//slice
+console.log("Slice ", arr.slice(1,3) , arr) // no change in array
+
+//splice
+console.log("Splice ", arr.splice(3,1) , arr) // change in array
