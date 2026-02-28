ffe55ba0fa393f1a41ebe773373034b471b1bb10
SHIVA KUMAR <151666163+Hpshiva@users.noreply.github.com>
2026-02-28 09:37:45 +0000

    Array with spread,isArray,flat,from

--- 02_basic/Array_02.js
+++ 02_basic/Array_02.js
@@ -4,11 +4,20 @@ let newarr=[...arr,...arr1] // spread [...val]
 console.log(newarr)
 
 
-let a1=[1,2,3,4,[5,6,[7,8,9,[10]]]] // flat add all in one with flat menothd 
-console.log(a1.flat(Infinity))
+let a1=[1,2,3,4,[5,6,[7,8,9,[10]]]] // flat add all in one with flat method 
+console.log(a1.flat(Infinity)) // flat add all in one with flat method //Infinity means all elements 
 
 
-console.log(Array.isArray(a1))
+console.log(Array.isArray(a1)) // boolean (true or flase) // is that an array true or false
 console.log(Array.from(newarr))   
 
 console.log(a1)
+
+
+
+// .of (its typeof is Object)
+let s1=100
+let s2=200
+let s3=300
+console.log(Array.of(s1,s2,s3)) 
+console.log(typeof (Array.of(s1,s2,s3))) 
