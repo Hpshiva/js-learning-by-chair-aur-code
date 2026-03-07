53057632074efd90e8ca3f44edfc0de864f6863a
SHIVA KUMAR <151666163+Hpshiva@users.noreply.github.com>
2026-03-07 09:05:50 +0000

    function basics

---
+++ 03_function/function_starting.js
@@ -0,0 +1,13 @@
+function my_name(name){               // function ke defination are parameters
+    console.log(`${name} is my name`) //backtick
+}
+my_name("lorem40")                    //function ko call are arguments
+
+
+
+function addsum(a,b){
+    let res= a+b
+    return res
+}
+const result=addsum(1,2)
+console.log(result)
