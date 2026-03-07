0da1b5e3dfce42e3306b2306d2bf7564617aa2ac
SHIVA KUMAR <151666163+Hpshiva@users.noreply.github.com>
2026-03-07 19:09:30 +0000

    iffe

---
+++ 03_function/iffe.js
@@ -0,0 +1,24 @@
+// IMMEDIATELY INVOKED FUNCTION EXPRESSIONS (IIFE)
+
+function iffe(){
+    console.log("iffe function expresstion which is Global variable pollution happens when too many variables are declared in the global scope, causing conflicts and overwriting. IIFE helps prevent this by creating a private scope.")
+}
+// iffe();
+
+// to make it iffe 
+
+// 1.basic way for iffe
+(function iffe_with_basic_way(){
+    console.log("basic way of iffe")
+})();
+
+// 2.iffe with arrow function 
+(()=>{
+    console.log("iffe with arrow function")
+})();
+
+
+// with parameter and arguments
+((name)=>{
+    console.log("iffe with arrow function" , `${name}`)
+})("shiva");
