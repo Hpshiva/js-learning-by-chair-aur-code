e7a151de20c3b5b6526c01f98ffb4397d901807e
SHIVA KUMAR <151666163+Hpshiva@users.noreply.github.com>
2026-03-08 15:41:49 +0000

    truthy_and_falsy

---
+++ 04_control_flow/truthy_and_falsy.js
@@ -0,0 +1,29 @@
+// falsy values
+
+// false , 0 , -0 , BigInt , "" , null , undefined ,NaN
+
+
+
+// truthy values
+// "0" , "false" , " " , [] , {} ,function(){}
+
+
+const userEmail="shivakumar.bfgi@gmail.com"
+// simple way
+if(userEmail)
+{
+    console.log("got user email")
+}
+else
+    console.log("not found ")
+
+
+// with array
+if(userEmail.length===0){
+    console.log("array is empty")
+}
+
+// with object
+if(Object.keys(userEmail).length===0){      // Object.keys will give an ARRAY
+    console.log("object is empty")
+}
