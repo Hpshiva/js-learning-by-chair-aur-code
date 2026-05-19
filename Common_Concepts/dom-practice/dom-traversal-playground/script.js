55c3412d46c9a4a3d59bd80c8435a74bd99dc81e
SHIVA KUMAR <151666163+Hpshiva@users.noreply.github.com>
2026-05-19 19:30:45 +0000

    dom-traversing

---
+++ Common_Concepts/dom-practice/dom-traversal-playground/script.js
@@ -0,0 +1,36 @@
+const grandparent = document.querySelector('.grandparent')
+const parent = document.querySelector('.parent')
+const childs = document.querySelectorAll('.child')
+
+
+// console.log(childs)
+
+// traverse elements
+for (let elem of childs) {
+    elem.addEventListener('click', () => {
+        //elem.closest(".grandparent").style.backgroundColor = "pink"
+    })
+}
+//  .parentElement = goes to parent of the element
+
+//  .closest = goes to parent of the element with extra benefits like
+// closest() doesn't care how many wrappers, buttons, or spans 
+// Your code breaks because.parentElement 
+// blindly moves up exactly one physical step, 
+// regardless of what that element actually is.
+
+
+// let traverse to childrens
+parent.addEventListener('click', () => {
+
+    // parent's first child Element 
+    parent.firstElementChild.style.backgroundColor = "yellow"
+
+    // parent's last child Element 
+    parent.lastElementChild.style.backgroundColor = "green"
+    // for (let elem of parent.children) {
+
+    //     elem.style.backgroundColor = "crimson"
+    //     console.log(elem)
+    // }
+})
